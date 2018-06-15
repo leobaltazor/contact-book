0220fe7e509fea1b11190651728e9817bac64a94
Leo <leo.baltazor@gmail.com>
2018-06-15 10:47:06 +0300

    server init

---
+++ back-end/index.js
@@ -0,0 +1,5 @@
+const app = require("express")();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const firebase = require("firebase");
+const cf = require("./config/config.json");
